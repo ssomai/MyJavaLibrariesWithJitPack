c2ddef269804d7ffc1c1b72adc5069d321e43224
ssomai <ssomai@gmail.com>
2019-05-18 18:45:20 +0900

    update

--- moduleJooqStrategy/src/main/java/com/ssomai/myjavalibraries/jooq/AsInDatabaseStrategy.java
+++ moduleJooqStrategy/src/main/java/com/ssomai/myjavalibraries/jooq/AsInDatabaseStrategy.java
@@ -4,24 +4,30 @@ package com.ssomai.myjavalibraries.jooq;
 import org.jooq.codegen.DefaultGeneratorStrategy;
 import org.jooq.codegen.GeneratorStrategy;
 import org.jooq.meta.Definition;
+import org.jooq.meta.IndexDefinition;
 
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by ssomai on 2/15/16.
- */
+import static java.util.Arrays.asList;
+import static org.jooq.SQLDialect.MARIADB;
+import static org.jooq.SQLDialect.MYSQL;
+
 
 public class AsInDatabaseStrategy extends DefaultGeneratorStrategy {
-
     /**
      * Override this to specifiy what identifiers in Java should look like.
      * This will just take the identifier as defined in the database.
      */
     @Override
     public String getJavaIdentifier(Definition definition) {
-        return definition.getOutputName();
+        // [#6307] Some databases work with per-table namespacing for indexes, not per-schema namespacing.
+        //         In order to have non-ambiguous identifiers, we need to include the table name.
+        if (definition instanceof IndexDefinition && asList(MYSQL, MARIADB).contains(definition.getDatabase().getDialect().family()))
+            return ((IndexDefinition) definition).getTable().getOutputName() + "_" + definition.getOutputName();
+        else
+            return definition.getOutputName();
     }
 
     /**
@@ -120,6 +126,120 @@ public class AsInDatabaseStrategy extends DefaultGeneratorStrategy {
     public String getOverloadSuffix(Definition definition, Mode mode, String overloadIndex) {
         return "_OverloadIndex_" + overloadIndex;
     }
-
-
 }
+
+
+///**
+// * Created by ssomai on 2/15/16.
+// * 0.9.5 까지만
+// */
+//
+//public class AsInDatabaseStrategy extends DefaultGeneratorStrategy {
+//    /**
+//     * Override this to specifiy what identifiers in Java should look like.
+//     * This will just take the identifier as defined in the database.
+//     */
+//    @Override
+//    public String getJavaIdentifier(Definition definition) {
+//        return definition.getOutputName();
+//    }
+//
+//    /**
+//     * Override these to specify what a setter in Java should look like. Setters
+//     * are used in TableRecords, UDTRecords, and POJOs. This example will name
+//     * setters "set[NAME_IN_DATABASE]"
+//     */
+//    @Override
+//    public String getJavaSetterName(Definition definition, GeneratorStrategy.Mode mode) {
+//        return "set" + definition.getOutputName();
+//    }
+//
+//    /**
+//     * Just like setters...
+//     */
+//    @Override
+//    public String getJavaGetterName(Definition definition, GeneratorStrategy.Mode mode) {
+//        return "get" + definition.getOutputName();
+//    }
+//
+//    /**
+//     * Override this method to define what a Java method generated from a database
+//     * Definition should look like. This is used mostly for convenience methods
+//     * when calling stored procedures and functions. This example shows how to
+//     * set a prefix to a CamelCase version of your procedure
+//     */
+//    @Override
+//    public String getJavaMethodName(Definition definition, Mode mode) {
+//        return "call" + org.jooq.tools.StringUtils.toCamelCase(definition.getOutputName());
+//    }
+//
+//    /**
+//     * Override this method to define how your Java classes and Java files should
+//     * be named. This example applies no custom setting and uses CamelCase versions
+//     * instead
+//     */
+//    @Override
+//    public String getJavaClassName(Definition definition, Mode mode) {
+//        //return super.getJavaClassName(definition, mode);
+////        if(definition.getSchema().)
+////        if(mode == Mode.RECORD) {
+////            return "TR"+definition.getOutputName().substring(1);
+////        }
+//        switch (mode) {
+//            case RECORD: {
+//                return "TR"+definition.getOutputName().substring(1);
+//            }
+//        }
+//        return definition.getOutputName();
+//    }
+//
+//    /**
+//     * Override this method to re-define the package names of your generated
+//     * artefacts.
+//     */
+//    @Override
+//    public String getJavaPackageName(Definition definition, Mode mode) {
+//        return super.getJavaPackageName(definition, mode);
+//    }
+//
+//    /**
+//     * Override this method to define how Java members should be named. This is
+//     * used for POJOs and method arguments
+//     */
+//    @Override
+//    public String getJavaMemberName(Definition definition, Mode mode) {
+//        return definition.getOutputName();
+//    }
+//
+//    /**
+//     * Override this method to define the base class for those artefacts that
+//     * allow for custom base classes
+//     */
+//    @Override
+//    public String getJavaClassExtends(Definition definition, Mode mode) {
+//        return Object.class.getName();
+//    }
+//
+//    /**
+//     * Override this method to define the interfaces to be implemented by those
+//     * artefacts that allow for custom interface implementation
+//     */
+//    @Override
+//    public List<String> getJavaClassImplements(Definition definition, Mode mode) {
+//        return Arrays.asList(Serializable.class.getName(), Cloneable.class.getName());
+//    }
+//
+//    /**
+//     * Override this method to define the suffix to apply to routines when
+//     * they are overloaded.
+//     *
+//     * Use this to resolve compile-time conflicts in generated source code, in
+//     * case you make heavy use of procedure overloading
+//     */
+//    @Override
+//    public String getOverloadSuffix(Definition definition, Mode mode, String overloadIndex) {
+//        return "_OverloadIndex_" + overloadIndex;
+//    }
+//
+//
+//}
